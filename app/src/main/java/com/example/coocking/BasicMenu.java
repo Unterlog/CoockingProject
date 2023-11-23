710256e93de62c1a1e43bff8d5ab613140eaa823
Unterlog <andrianounterlog@gmail.com>
2023-11-24 01:19:42 +0700

    Version 1.0.4 added dialog window

--- app/src/main/java/com/example/coocking/BasicMenu.java
+++ app/src/main/java/com/example/coocking/BasicMenu.java
@@ -2,13 +2,29 @@ package com.example.coocking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class BasicMenu extends AppCompatActivity {
 
+    Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_menu);
+    }
+    public void onClick(View view) {
+        showAddProduct();
+    }
+    public void showAddProduct() {
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialogaddproduct);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 }
